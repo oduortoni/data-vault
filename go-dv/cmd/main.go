0b8bb83afe1509399a6596bcb9dd22511a057e3e
toni-od <samaelsamael662@gmail.com>
2025-06-28 12:13:24 +0300

    feat: log file
    add a way to write data to the log file

--- go-dv/cmd/main.go
+++ go-dv/cmd/main.go
@@ -3,35 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 var (
-	Port int = 9000
+	Port int    = 9000
 	Host string = "0.0.0.0"
 )
 
 func main() {
 	fmt.Printf("Server listening on %s:%d\n", Host, Port)
-	
-	server, err := StartServer(Host, Port, "")
+
+	server, err := StartServer(Host, Port, "errors.log")
 	if err != nil {
-		server.Info(err, "could not start server", false)
+		server.Info(err, "could not start server", true)
 	}
 }
 
 type Server struct {
-	Host string
-	Port int
+	Host     string
+	Port     int
 	FileName string
-	Errors []string
+	Errors   []string
 }
 
 func StartServer(host string, port int, filename string) (*Server, error) {
 	server := &Server{
-		Host: host,
-		Port: port,
+		Host:     host,
+		Port:     port,
 		FileName: filename,
 	}
+
+	server.Info(nil, "some error", true)
 
 	err := server.ListenAndServe()
 
@@ -45,13 +48,38 @@ func (s Server) ListenAndServe() (err error) {
 }
 
 func (s Server) Info(err error, msg string, save bool) {
+	logMsg := fmt.Sprintf("[INFO] %s: %v\n", msg, err)
+	fmt.Print(logMsg)
+
 	if save {
-		//save to log file
+		s.appendToFile(logMsg)
 	}
 }
 
 func (s *Server) Panic(err error, msg string, save bool) {
+	logMsg := fmt.Sprintf("[PANIC] %s: %v\n", msg, err)
+	fmt.Fprint(os.Stderr, logMsg)
+
 	if save {
-		// save to file
+		s.appendToFile(logMsg)
+	}
+
+	os.Exit(1)
+}
+
+func (s *Server) appendToFile(logMsg string) {
+	if s.FileName == "" {
+		return
+	}
+
+	f, err := os.OpenFile(s.FileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open log file: %v\n", err)
+		return
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(logMsg); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to write to log file: %v\n", err)
 	}
 }
